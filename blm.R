c168fb86f13e350f8cee118e70dfb577965407ce
Steve Chall <stevec@renci.org>
2017-02-24 16:16:42 -0500

    more comment cleanup

--- blm.R
+++ blm.R
@@ -7,8 +7,8 @@
 # "map", then create a "binary link matrix" as a data-frame of dimension n, where
 # n is the number of nodes in the input .json file. The rows and columns of the
 # data frame are named by the node names "map$nodes$name". The value of m[i, j]
-# is 1 if there is a link between node[i] and node[j], otherwise 0. For
-# each file write 1) the blm to a csv output file, along with 2) a list of nodes
+# is 1 if there is a link between node[i] and node[j], otherwise 0. For # each
+# file write 1) the blm to a csv output file, along with 2) a list of nodes
 # showing id, type (role, responsibility, etc.), and name (associated text), and
 # 3) a list of links showing source and target node ids and name (i.e., the text
 # associated with that link).
